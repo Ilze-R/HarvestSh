10bc68da8ca1246c5bc70d6260e7a0aa6b7b70b3
Ilze Rigerte <irigerte@gmail.com>
2023-05-30 22:52:43 +0100

    before angular update

--- src/app/give-dashboard/give-dashboard.component.ts
+++ src/app/give-dashboard/give-dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../_services/authentication.service';
+import { User } from '../_models/user.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-give-dashboard',
@@ -7,9 +10,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./give-dashboard.component.scss'],
 })
 export class GiveDashboardComponent implements OnInit {
-  constructor(private router: Router) {}
+  currentUser!: User | null;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
   ngOnInit(): void {
+    console.log(this.currentUser?.username + 'first');
     this.router.navigate(['/give-dashboard']);
+    this.authenticationService.currentUser$.subscribe((user) => {
+      console.log(JSON.stringify(user));
+      this.currentUser = user;
+      console.log(this.currentUser?.username + 'second');
+    });
+    console.log(this.currentUser?.username + 'third');
   }
 }
